8328b50f80b5d623fc6e65a22b103f0499cd9116
Steffi Tan <steffitlw@berkeley.edu>
2022-06-23 22:38:51 -0700

    Add course info and staff pages

--- src/components/CourseInfoPage/CourseInfo.js
+++ src/components/CourseInfoPage/CourseInfo.js
@@ -1,10 +1,20 @@
-
+import { useState, useEffect } from 'react'
+import ReactMarkdown from "react-markdown";
+import gfm from "https://cdn.skypack.dev/remark-gfm@1.0.0";
+import "./courseinfo.css"
 
 function CourseInfoPage() {
+  const [content, setContent] = useState("");
 
+    useEffect(() => {
+        fetch("content/course-info.md")
+        .then((res) => res.text())
+        .then((text) => setContent(text));
+    }, []);
   return (
-    <div className="Navbar">
-      hi
+    <div className="text-wrapper">
+      <h1 className="heading">Course Information</h1>
+      <ReactMarkdown children={content} remarkPlugins={[gfm]}/>
     </div>
   );
 }
