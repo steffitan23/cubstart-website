bda0f138f5dc1d82d15247157e65438c4a943b96
Steffi Tan <steffitlw@berkeley.edu>
2022-10-16 13:06:36 -0700

    Update InfoSection.js

--- src/components/HomePage/InfoSection/InfoSection.js
+++ src/components/HomePage/InfoSection/InfoSection.js
@@ -8,7 +8,7 @@ function InfoSection(props) {
             <h2>What is Cubstart?</h2>
             <p>Cubstart is a student-run course as part of the <b><a href="https://decal.berkeley.edu/about/decal-program">DeCal Program</a></b> at University of California, Berkeley. We're run by members of <b><a href="https://calhacks.io">Cal Hacks</a></b>, the world's largest collegiate hackathon. We've been running since the Spring 2021 semester, with the aim of supporting underrepresented minorities in their journeys in STEM. </p>
 
-            <p>We have 2 tracks, iOS development and web development, so students can choose the platform they prefer!</p>
+            <p>We have 2 tracks, <b>iOS</b> development and <b>Web</b> development, so students can choose the platform they prefer!</p>
 
             <img src="assets/cubstart-discussion.jpg" id="cubstart-img"></img>
 
