91a8207a46ad6ed01dcd1707fc73684cfad614eb
Steffi Tan <steffitlw@berkeley.edu>
2022-10-19 22:06:01 -0700

    Add swe

--- src/components/HomePage/DescSection/DescSection.js
+++ src/components/HomePage/DescSection/DescSection.js
@@ -7,8 +7,11 @@ function DescSection(props) {
             <div className="desc-text">
             <h2>Our Partners</h2>
             <p>We are determined to support underrepresented minorities in STEM. This semester, we're grateful for our partner organisations who help us get closer to our mission of bridging the gaps in STEM.</p>
-            <a href="https://hes.berkeley.edu/"><img src="assets/hes_logo.png" className="partner-logo hes">{props.children}</img></a>
-            <a href="http://www.ucberkeleybessa.com/"><img src="assets/bessa_logo.png" className="partner-logo bessa">{props.children}</img></a>
+            <div className="sponsor-wrapper">
+                <a href="https://hes.berkeley.edu/"><img src="assets/hes_logo.png" className="partner-logo hes">{props.children}</img></a>
+                <a href="http://www.ucberkeleybessa.com/"><img src="assets/bessa_logo.png" className="partner-logo bessa">{props.children}</img></a>
+                <a href="https://swe.berkeley.edu/"><img src="assets/swe_logo.png" className="partner-logo swe">{props.children}</img></a>
+            </div>
             </div>
         </div>
     );
